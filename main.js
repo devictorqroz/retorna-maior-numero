fd4f9cdb2a8ecde12867430761ba61eb888573d6
Victor Queiroz <victorqueiroz.ti@gmail.com>
2023-03-02 15:15:48 -0300

    adicionando projeto

---
+++ main.js
@@ -0,0 +1,29 @@
+
+var numeros = [];
+var i = 0;
+
+var valorDigitado = document.getElementById("valorDigitado");
+var botaoGravarValor = document.getElementById("gravarValor");
+var botaoresultado = document.getElementById("resultado");
+
+function adicionarNumero() {
+  numeros[i] = parseInt(valorDigitado.value);
+  i++;
+  valorDigitado.value = " ";
+}
+
+botaoGravarValor.onclick = adicionarNumero;
+
+function retornaMaiorNumero() {
+  var maiorNumero = 0;
+
+  for(var i = 0; i<numeros.length; i++) {
+    if(maiorNumero < numeros[i]) {
+      maiorNumero = numeros[i];
+    }
+  }
+
+  document.write("O maior valor contido no array é: " + maiorNumero);
+}
+
+resultado.onclick = retornaMaiorNumero;
